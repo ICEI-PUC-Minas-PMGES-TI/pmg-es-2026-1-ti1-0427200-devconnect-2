e356a136538ab0154152d32effa47c8f3cc4e668
Larissa Fineli <larissa4321ramos@gmail.com>
2026-06-24 19:16:39 -0300

    botao topo js

--- codigo/public/modulos/perfil/script.js
+++ codigo/public/modulos/perfil/script.js
@@ -172,3 +172,20 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     }
 });
+
+const btnTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 250) {
+        btnTopo.style.display = "block";
+    } else {
+        btnTopo.style.display = "none";
+    }
+});
+
+btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
