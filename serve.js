eb954d293381da01262a3f4cbdfdcaf0dbd2f406
Erick <erick.calixtods@gmail.com>
2026-06-28 14:00:27 -0300

    fix: adiciona server.js para gerenciar rotas e porta

---
+++ serve.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db/db.json');
+const middlewares = jsonServer.defaults({ static: './public' });
+
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server está rodando na porta ${port}`);
+});
